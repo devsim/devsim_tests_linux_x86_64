abb94ccc4dd2532964d777af50fa6976e53e1d88
Juan E. Sanchez <tcaduser@github>
2017-05-06 01:21:45 +0000

    get tests running

--- travis_tests.sh
+++ travis_tests.sh
@@ -2,7 +2,7 @@
 set -e
 BASEDIR="${PWD}"
 TAG=${1}
-TAGDIR=devsim_osx_${TAG}
+TAGDIR=devsim_linux_${TAG}
 TAGTGZ=${TAGDIR}.tgz
 DEVSIM_PY=${TAGDIR}/bin/devsim
 DEVSIM_TCL=${TAGDIR}/bin/devsim_tcl
@@ -10,6 +10,13 @@ ANACONDA_PATH=${HOME}/anaconda
 
 #curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
 #tar xzf ${TAGTGZ} 
+yum install -y epel-release
+yum install -y cmake3
+
+if [ ! -d ${HOME}/anaconda ]; then
+( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh  && bash Miniconda2-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
+${HOME}/anaconda/bin/conda install -y numpy mkl
+fi
 
 mkdir -p bin
 
@@ -19,7 +26,7 @@ set -e
 progname="\$0"
 curdir=\`dirname "\$progname"\`
 ANACONDA_PATH=${ANACONDA_PATH}
-export DYLD_INSERT_LIBRARIES=\${ANACONDA_PATH}/lib/libpython2.7.dylib:\${ANACONDA_PATH}/lib/libmkl_rt.dylib
+export LD_LIBRARY_PATH=\${ANACONDA_PATH}/lib
 export PYTHONHOME=\${ANACONDA_PATH}
 # sequential really speeds things up
 export MKL_NUM_THREADS=1
@@ -33,7 +40,7 @@ set -e
 progname="\$0"
 curdir=\`dirname "\$progname"\`
 ANACONDA_PATH=${ANACONDA_PATH}
-export DYLD_INSERT_LIBRARIES=\${ANACONDA_PATH}/lib/libtcl8.5.dylib:\${ANACONDA_PATH}/lib/libmkl_rt.dylib
+export LD_LIBRARY_PATH=\${ANACONDA_PATH}/lib
 export TCL_LIBRARY=\${ANACONDA_PATH}/lib/tcl8.5
 # sequential really speeds things up
 export MKL_NUM_THREADS=1
@@ -45,5 +52,5 @@ ln -sf ${TAGDIR}/testing .
 ln -sf ${TAGDIR}/examples .
 
 rm -rf run && mkdir run
-(cd run && cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY_TEST_EXE=${BASEDIR}/bin/devsim -DDEVSIM_TCL_TEST_EXE=${BASEDIR}/bin/devsim_tcl ..)
-(cd run && ctest -j2)
+(cd run && cmake3 -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY_TEST_EXE=${BASEDIR}/bin/devsim -DDEVSIM_TCL_TEST_EXE=${BASEDIR}/bin/devsim_tcl ..)
+(cd run && ctest3 -j2)
