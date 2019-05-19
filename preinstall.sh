27d594cae04e17b53915ac215cc576d9e2d733e0
Juan E. Sanchez <juan@tcad.com>
2019-05-18 21:29:04 -0500

    Tcl to Python conversion

--- preinstall.sh
+++ preinstall.sh
@@ -4,11 +4,10 @@ set -e
 yum install -y perl
 
 if [ ! -d ${HOME}/anaconda ]; then
-( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh  && bash Miniconda2-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
+( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  && bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
 fi
 
-${HOME}/anaconda/bin/conda install -y --name base mkl cmake
-${HOME}/anaconda/bin/conda create  -y --name python27_devsim python=2.7
-${HOME}/anaconda/bin/conda install -y --name python27_devsim numpy mkl cmake
+${HOME}/anaconda/bin/conda install -y --name base cmake
 ${HOME}/anaconda/bin/conda create  -y --name python37_devsim python=3.7
 ${HOME}/anaconda/bin/conda install -y --name python37_devsim numpy mkl
+
