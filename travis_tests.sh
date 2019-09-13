1e794fa59b3ffd2a96a0b6ddb7156338856c1abf
Juan E. Sanchez <juan@tcad.com>
2019-09-12 20:47:12 -0500

    fix scripts

--- travis_tests.sh
+++ travis_tests.sh
@@ -4,34 +4,12 @@ BASEDIR="${PWD}"
 TAG=${1}
 TAGDIR=devsim_linux_${TAG}
 TAGTGZ=${TAGDIR}.tgz
-#DEVSIM_PY=${TAGDIR}/bin/devsim
-#DEVSIM_PY3=${TAGDIR}/bin/devsim_py3
-DEVSIM_TCL=${TAGDIR}/bin/devsim_tcl
 DEVSIM_LIB=${TAGDIR}/lib
 ANACONDA_PATH=${HOME}/anaconda
 
 #curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
 #tar xzf ${TAGTGZ} 
-UTILITY_PATH=$(source ${ANACONDA_PATH}/bin/activate python27_devsim && echo ${CONDA_PREFIX}/bin)
-
-mkdir -p bin
-(cd bin && ln -sf ${UTILITY_PATH}/cmake && ln -sf ${UTILITY_PATH}/ctest)
-
-cat << EOF > bin/devsim_py27
-#!/bin/bash
-set -e
-progname="\$0"
-curdir=\`dirname "\${progname}"\`
-ANACONDA_PATH=${ANACONDA_PATH}
-export PYTHONHASHSEED=0
-# sequential speeds up small examples
-export MKL_NUM_THREADS=1
-source \${ANACONDA_PATH}/bin/activate python27_devsim
-export LD_LIBRARY_PATH=\${CONDA_PREFIX}/lib
-export PYTHONPATH="\${curdir}"/../${DEVSIM_LIB}
-python "\$@"
-EOF
-chmod +x bin/devsim_py27
+UTILITY_PATH=$(source ${ANACONDA_PATH}/bin/activate python37_devsim && echo ${CONDA_PREFIX}/bin)
 
 cat << EOF > bin/devsim_py37
 #!/bin/bash
@@ -48,24 +26,10 @@ python "\$@"
 EOF
 chmod +x bin/devsim_py37
 
-cat << EOF > bin/devsim_tcl
-#!/bin/bash
-set -e
-progname="\$0"
-curdir=\`dirname "\${progname}"\`
-ANACONDA_PATH=${ANACONDA_PATH}
-export LD_LIBRARY_PATH=\${ANACONDA_PATH}/lib
-export TCL_LIBRARY=\${ANACONDA_PATH}/lib/tcl8.6
-# sequential speeds up small examples
-export MKL_NUM_THREADS=1
-\${curdir}/../${DEVSIM_TCL} "\$@"
-EOF
-chmod +x bin/devsim_tcl
-
 ln -sf ${TAGDIR}/testing .
 ln -sf ${TAGDIR}/examples .
 
 rm -rf run && mkdir run
-(cd run && ../bin/cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY2_TEST_EXE=${BASEDIR}/bin/devsim_py27 -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py37 -DDEVSIM_TCL_TEST_EXE=${BASEDIR}/bin/devsim_tcl ..)
+(cd run && ../bin/cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py37 ..)
 (cd run && ../bin/ctest -j4)
 
