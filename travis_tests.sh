bf380ea3c5cab0d56f7543a76579f5946bf1b24f
Juan E. Sanchez <juan@tcad.com>
2019-09-18 23:28:30 -0500

    update script

--- travis_tests.sh
+++ travis_tests.sh
@@ -5,7 +5,6 @@ TAG=${1}
 TAGDIR=devsim_linux_${TAG}
 TAGTGZ=${TAGDIR}.tgz
 DEVSIM_LIB=${TAGDIR}/lib
-ANACONDA_PATH=${HOME}/anaconda
 
 #curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
 #tar xzf ${TAGTGZ} 
@@ -15,11 +14,9 @@ cat << EOF > bin/devsim_py37
 set -e
 progname="\$0"
 curdir=\`dirname "\${progname}"\`
-ANACONDA_PATH=${ANACONDA_PATH}
 export PYTHONHASHSEED=0
 # sequential speeds up small examples
 export MKL_NUM_THREADS=1
-source \${ANACONDA_PATH}/bin/activate python37_devsim
 export PYTHONPATH="\${curdir}"/../${DEVSIM_LIB}
 python "\$@"
 EOF
