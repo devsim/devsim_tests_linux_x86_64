5fb419d0773d18f064a75fd28730c008d20ba836
Juan E. Sanchez <juan@tcad.com>
2020-07-19 21:42:55 -0500

    update

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,7 @@ set -e
 yum install -y perl
 
 if [ ! -d ${HOME}/anaconda ]; then
-( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh  && bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
+( cd ${HOME} && curl -L -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh  && bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
 fi
 
 ${HOME}/anaconda/bin/conda install -y --name base cmake
